b692cc0eceaf68e91cbd20e9b3b1993be8d9f463
emon5419 <alauddinemon3@gmail.com>
2023-03-26 22:21:12 +0600

    BanglaVersion-CakeCardAdd

--- src/Pages/Home/Home1.js
+++ src/Pages/Home/Home1.js
@@ -13,21 +13,21 @@ const Home1 = () => {
             <div className="container max-w-xl px-3 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
                 <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center text-start">
                     <div>
-                        <p className='lg:text-3xl text-2xl  text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-cyan-600 mb-3 to-pink '>Welcome to</p>
+                        <p className='lg:text-3xl text-2xl  text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-cyan-600 mb-3 to-pink '>স্বাগতম</p>
 
-                        <h2 className=' font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-600mb-6 to-purple-700  text-left font-logo text-gray-900 text-2xl lg:text-4xl '>
-                            CARRYwithCARE 2.0<br /> <span className='lg:text-2xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-cyan-600 mb-6 to-pink'>Premium <span className='bg-clip-text bg-gradient-to-r from-cyan-500 to-purple-600 mb-6 to-pink'>Delivery Service</span></span>
+                        <h2 className=' font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400 mb-6 to-purple-700  text-left font-logo text-gray-900 text-2xl lg:text-4xl '>
+                            CARRYwithCARE 2.0<br /> <span className='lg:text-2xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400 mb-6 to-pink'>প্রিমিয়াম <span className='bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400 mb-6 to-pink'>ডেলিভারি সার্ভিস</span></span>
                         </h2>
 
-                        <p className="mt-3 text-lg text-gray-600">The very first Premium Home Delivery Service in Chittagong. We have an experienced & professional delivery team to deliver your product just on time! </p>
+                        <p className="mt-3 text-lg text-gray-600">চট্টগ্রামে প্রথম প্রিমিয়াম হোম ডেলিভারি সার্ভিস। ঠিক সময়ে আপনার পণ্য সরবরাহ করার জন্য আমাদের একটি অভিজ্ঞ এবং পেশাদার দল রয়েছে!</p>
 
                         <div className='flex flex-col items-center md:flex-row mt-3' >
                             <Link
-                                to='/earn-with-bike'
+                                to='https://www.facebook.com/CarrywithCare2.0'
                                 className='inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-gradient-to-r from-cyan-400 to-purple-500 hover:from-cyan-500 hover:to-cyan-500 focus:shadow-outline focus:outline-none'
                             >
                                 < FaMoneyBillAlt />
-                                <span className='ml-2 mr-3 lg:mr-6'> Deliver & Earn</span>
+                                <span className='ml-2 mr-3 lg:mr-6'>Deliver your Parcel</span>
                                 <FaArrowRight />
                             </Link>
 
--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Lottie from 'lottie-react'
 import Food from '../../assets/fast-food.json'
 import Gift from '../../assets/gift-boxes.json'
+import cake from '../../assets/cake.json';
 import Document from '../../assets/document-icon-lottie-animation.json'
 import ServiceDetails from './ServiceDetails';
 
@@ -12,25 +13,32 @@ const Service = () => {
 			<section className="py-2 text-gray-900">
 				<div className="container mx-auto sm:p-10">
 					<div className="mb-16 space-y-4 text-center">
-						<h1 className="text-3xl font-logo  leading-tight">What Can You Send?</h1>
-						<p className="px-4 sm:px-8 lg:px-24">We offer a wide range of delivery services to meet your needs. Whether you're looking to send a parcel, an official document, or a special surprise, we have you covered. Here are some of our key services:</p>
+						<h1 className="text-3xl font-logo  leading-tight">আপনি কি পাঠাতে পারেন?</h1>
+						<p className="px-4 sm:px-8 lg:px-24">আমরা আপনার চাহিদা মেটাতে বিস্তৃত পরিসরের ডেলিভারি পরিষেবা অফার করি। আপনি কি পার্সেল, একটি অফিসিয়াল ডকুমেন্টস, বা একটি স্পেশাল সারপ্রাইস পাঠাতে কাউকে খুঁজছেন?
+							এখানে আমাদের কিছু গুরুত্বপূর্ণ পরিষেবা রয়েছে:</p>
 
 					</div>
-					<div className="grid max-w-md grid-cols-1 gap-6 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-3 px-3 ">
+					<div className="grid max-w-md grid-cols-1 gap-6 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-4 px-3 ">
 						<div className="relative z-0 flex flex-col items-center p-8 border-2 hover:border-red-400   rounded-md">
 							<span className="absolute top-0 px-6 pt-1 pb-2 font-medium rounded-b-lg bg-gradient-to-r from-green-400 to-blue-500 text-gray-50">FOODS</span>
 							<span><Lottie animationData={Food} loop={true} /></span>
 
 						</div>
 						<div className="relative z-0 flex flex-col items-center p-8 border-2 hover:border-red-400   rounded-md">
-							<span className="absolute top-0 px-6 pt-1 pb-2 font-medium rounded-b-lg bg-gradient-to-r from-green-400 to-blue-500 text-gray-50">GIFT</span>
-							<span className='mt-3'><Lottie animationData={Gift} loop={true} /></span>
-
+							<span className="absolute top-0 px-6 pt-1 pb-2 font-medium rounded-b-lg bg-gradient-to-r from-green-400 to-blue-500 text-gray-50">Cake & Dessert</span>
+							<span><Lottie animationData={cake} loop={true} /></span>
 						</div>
 						<div className="relative z-0 flex flex-col items-center p-8 border-2 hover:border-red-400   rounded-md">
 							<span className="absolute top-0 px-6 pt-1 pb-2 font-medium rounded-b-lg bg-gradient-to-r from-green-400 to-blue-500 text-gray-50">DOCUMENT</span>
 							<span><Lottie animationData={Document} loop={true} /></span>
 						</div>
+						<div className="relative z-0 flex flex-col items-center p-8 border-2 hover:border-red-400   rounded-md">
+							<span className="absolute top-0 px-6 pt-1 pb-2 font-medium rounded-b-lg bg-gradient-to-r from-green-400 to-blue-500 text-gray-50">GIFT</span>
+							<span className='mt-3'><Lottie animationData={Gift} loop={true} /></span>
+
+						</div>
+
+
 					</div>
 					<ServiceDetails></ServiceDetails>
 				</div>
